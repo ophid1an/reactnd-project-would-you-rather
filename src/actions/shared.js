29f243a7b797d1fe77b64164345e9b035190eb61
Giannis Dermentzoglou <giannis@codeseed.io>
2020-05-20 18:57:35 +0300

    mock authed user

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,9 +1,13 @@
 import { getInitialData } from "../utils/api";
 import {getUsers} from "./users";
 import {getQuestions} from "./questions";
+import {loginUser} from "./authedUser";
+
+const USER_ID = 'johndoe';
 
 export function getData() {
   return dispatch => {
+    dispatch(loginUser(USER_ID));
     return getInitialData()
       .then(({ users, questions }) => {
         dispatch(getUsers(users));
