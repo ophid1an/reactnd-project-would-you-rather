5b476aa8023aba3e27b1ab30f94b35e05b35662f
Giannis Dermentzoglou <giannis@codeseed.io>
2020-05-21 14:20:07 +0300

    fix authedUser reducer

--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -3,7 +3,7 @@ import {LOGIN_USER, LOGOUT_USER} from "../actions/authedUser";
 export default function authedUser(state = null, action) {
   switch (action.type) {
     case LOGIN_USER:
-      return {id: action.id};
+      return action.id;
     case LOGOUT_USER:
       return null;
     default:
